3a2f163b5e5a16ee740bdfdbc97044398c9884d9
LeonardoPenas <n00460162@upn.pe>
2026-09-16 16:51:12 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/praactica_de_campo_semana5/Praactica_De_Campo_Semana5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praactica_de_campo_semana5;
+
+/**
+ *
+ * @author leore
+ */
+public class Praactica_De_Campo_Semana5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
