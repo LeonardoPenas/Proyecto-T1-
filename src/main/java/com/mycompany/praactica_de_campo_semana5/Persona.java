7505ad63da42edcec1b4a3c66d834abaa2a2920a
Leonardo Chávez <N00449821@upn.pe>
2026-09-17 16:30:28 -0500

    Creacion de clase persona

---
+++ src/main/java/com/mycompany/praactica_de_campo_semana5/Persona.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.praactica_de_campo_semana5;
+
+/**
+ *
+ * @author leoch
+ */
+public class Persona 
+{
+    
+}
