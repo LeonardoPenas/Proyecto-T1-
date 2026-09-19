8cc654c2e020a2602d8ec8b0ddda7962935d4306
Leonardo Chávez <N00449821@upn.pe>
2026-09-19 14:12:01 -0500

    creacion de sobrecarga de constructores

--- src/main/java/com/mycompany/praactica_de_campo_semana5/Persona.java
+++ src/main/java/com/mycompany/praactica_de_campo_semana5/Persona.java
@@ -12,5 +12,58 @@ public class Persona
 {
     private String Nombre;
     private String Apellidos;
+    private String cod;
     private String dni;
+
+    public Persona() {
+    }
+
+    public Persona(String Nombre, String Apellidos, String cod) {
+        this.Nombre = Nombre;
+        this.Apellidos = Apellidos;
+        this.cod = cod;
+    }
+
+    public Persona(String Nombre, String Apellidos, String cod, String dni) {
+        this.Nombre = Nombre;
+        this.Apellidos = Apellidos;
+        this.cod = cod;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+    
+    
+    
 }
