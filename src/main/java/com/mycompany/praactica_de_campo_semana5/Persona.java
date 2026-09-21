a7b5d733e48dcd54582fae6f2e1390452befc206
Elvis_M <maynorelvisjcm@gmail.com>
2026-09-21 02:38:33 -0500

    Conflicto resuelto y nueva clase -Direccion-

--- src/main/java/com/mycompany/praactica_de_campo_semana5/Persona.java
+++ src/main/java/com/mycompany/praactica_de_campo_semana5/Persona.java
@@ -12,5 +12,109 @@ public class Persona
 {
     private String Nombre;
     private String Apellidos;
+    private String cod;
     private String dni;
+    private int edad;
+    private Direccion direccion;
+
+    public Persona() {
+    }
+
+    public Persona(String Nombre, String Apellidos, String cod) {
+        this.Nombre = Nombre;
+        this.Apellidos = Apellidos;
+        this.cod = cod;
+    }
+
+    public Persona(String Nombre, String Apellidos, String cod, String dni, int edad) {
+        this.Nombre = Nombre;
+        this.Apellidos = Apellidos;
+        this.cod = cod;
+        this.dni = dni;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+    public void setCod(int cod) {
+        this.cod = Integer.toString(cod);
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public Boolean setDni(String dni) 
+    {
+        if(dni.length()==8)
+        {
+            this.dni= dni;
+            return true;
+        }else
+        {
+            System.out.println("ERROR, ingrese una cantidad valida ");
+            return false;
+        }
+    }
+    public Boolean setDni(int dni) 
+    {
+        return setDni(Integer.toString(dni));
+    }
+    public String getedad() {
+        return cod;
+    }
+
+    public void setedad(int edad) 
+    {
+        if (edad >=0 && edad<100) 
+        {
+            this.edad=edad;
+        }
+        else
+        {
+            System.out.println("EDAD INVALIDA");
+            return ;
+        }
+    }
+    public Direccion getDireccion()
+    {
+        return direccion;
+    }
+    public void setDireccion(Direccion direccion)
+    {
+        this.direccion = direccion;
+    }
+    
+
+    public void VerDatos()
+    {
+        System.out.println("Nombre: "+this.Nombre+" APELLIDO: "+this.Apellidos+ " CODIGO: "+this.cod+" DNI: "+this.dni+" EDAD: "+this.edad);
+        System.out.println("");
+        System.out.println("Direccion:");
+        this.direccion.VerDireccion();
+        System.out.println("");
+    }  
+    
+
 }
--- src/main/java/com/mycompany/praactica_de_campo_semana5/Praactica_De_Campo_Semana5.java
+++ src/main/java/com/mycompany/praactica_de_campo_semana5/Praactica_De_Campo_Semana5.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.praactica_de_campo_semana5;
+import java.util.Scanner;
 
 /**
  *
@@ -13,6 +14,61 @@ public class Praactica_De_Campo_Semana5 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hola Gentita-Cambio Elvis");
+        Scanner sc = new Scanner(System.in);
+        String rpta = "s";
+        PersonaController controla = new PersonaController();
 
+
+
+        while (rpta.equals("s"))
+        {
+            
+            try 
+            {
+             Persona p = new  Persona();
+            System.out.println("INGRESE NOMBRE DE PERSONA: ");
+            String nom = sc.nextLine();
+            p.setNombre(nom);
+
+            System.out.println("INGRESE APELLIDO: ");
+            String Ap = sc.nextLine();
+            p.setApellidos(Ap);
+
+            System.out.println("INRGESE CÓDIGO: ");
+            String cod=sc.nextLine();
+            p.setCod(cod);
+            
+            System.out.println("INGRESE DNI: ");
+            String dni =sc.nextLine();
+            p.setDni(dni);
+
+            try 
+            {
+                System.out.println("INGRESE EDAD: ");
+                int edad = Integer.parseInt(sc.nextLine());
+                p.setedad(edad);   
+            } catch (NumberFormatException e) {
+                System.out.println("ERROR: la edad debe ser un numero entero");
+            }
+            System.out.println("Direccion");
+            System.out.println("Ingrese calle");
+            String calle = sc.nextLine();
+            System.out.println("Ingrese ciudad");
+            String ciudad = sc.nextLine();
+            System.out.println("Ingrese distrito");
+            String distrito = sc.nextLine();
+            Direccion d = new Direccion(calle,ciudad,distrito);
+            p.setDireccion(d);
+            controla.agregar_persona(p);
+              
+            } catch (IllegalArgumentException e) {
+                System.out.println("ERROR: " + e.getMessage());
+                System.out.println("La persona NO fue registrada");
+            }
+            System.out.println("DESEA AÑADIR MÁS TRABAJADORES?");
+            rpta=sc.nextLine();
+            
+        }
+        controla.lista_persona();
     }
 }
